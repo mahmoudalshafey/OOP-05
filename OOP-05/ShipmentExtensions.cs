b25c741cb4a9ceedb51c3e697c70d54e24ebf971
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-24 11:56:58 +0300

    Extension Methods

---
+++ OOP-05/ShipmentExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_05
+{
+    internal static class ShipmentExtensions
+    {
+        public static string GetSummary(this Shipment shipment)
+        {
+            string shipmentType = shipment.GetType().Name.Replace("Shipment", "");
+            return $"{shipment.TrackingCode} | {shipmentType} | {shipment.Weight} KG | {shipment.TrackingStatus}";
+        }
+
+        public static bool IsDelivered(this Shipment shipment)
+        {
+            return shipment.TrackingStatus == "Delivered";
+        }
+    }
+}
